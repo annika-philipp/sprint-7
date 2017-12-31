25150f0125f4f6b7f852ede941f3b41fdf663af1
Annika Philipp <annika@Annikas-MacBook-Air.local>
2018-01-01 11:01:33 +1300

    recursion

--- eloquent.js
+++ eloquent.js
@@ -12,3 +12,25 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+
+
+Recursion
+
+function isEven (num) {
+    if (num == 0) {
+      return true;  //even = true
+    } else if (num == 1) {
+       return false;  //odd = false
+    } else if (num > 1) {
+          return isEven (num-2); // recursive part for pos number
+    } else if (num < 0) {
+      return isEven (num+2);  // recursive part for neg number
+    }
+   };
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-8));
+// → true
